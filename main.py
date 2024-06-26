a5ee0266d152a560d99adb0eaf58094184afc62e
MatttthewButel <124315158+MatttthewButel@users.noreply.github.com>
2024-06-26 12:06:21 +1200

    Adding images

--- main.py
+++ main.py
@@ -1,13 +1,35 @@
 """
 Main.
 
-Created by: NAME
-Date: DATE
+Created by: Matthew Butel & Sid McQueen
+Date: 17 June - Present
 """
 
-# Enter your code here
-name: str = input("Enter your name: ")
-if name:
-    print(f"Hello, {name}!")
-else:
-    print("Error getting name.")
+# Imports
+import remi.gui as GUI
+from remi import start, App
+
+
+class UI(App):
+    """The UI for the flash cards menu."""
+    
+    def __init__(self, *args):
+        """Make the app work."""
+        import os
+
+        abs_path: str = os.path.abspath(__file__)
+        dir_name: str = os.path.dirname(abs_path)
+        res_path: str = os.path.join(dir_name, "res")
+        super().__init__(*args, static_file_path={"res": res_path})
+    
+    def main(self):
+        """Run the site."""
+    
+    def home_screen(self):
+        """GUI for the home screen"""
+        
+        self.home_screen_title: GUI.Label = GUI.Label("Auto Bazaar")
+        self.image = GUI.Image("/res:image.avif")
+
+
+start(UI)
