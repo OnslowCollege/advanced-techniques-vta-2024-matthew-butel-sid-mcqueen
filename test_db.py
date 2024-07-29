8ab18575382fa391a045ec7acb0fdce4b761bedb
McQceen <124315125+McQceen@users.noreply.github.com>
2024-07-29 22:54:02 +0000

    Jul 29, 2024, 10:54 PM

--- test_db.py
+++ test_db.py
@@ -1,5 +1,16 @@
 """OKOKOKOK."""
 
 from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import relationship
+from sqlalchemy.orm import Base
 
-engine = create_engine("postgrespl+pypostgrespl:///:memory:", echo=True)
+pg_engine = create_engine(
+    "posthgresql://postgres:123456@localhost:5432/postgres"
+)
+
+
+class Cars(Base):
+    __tablename__ = "cars"
