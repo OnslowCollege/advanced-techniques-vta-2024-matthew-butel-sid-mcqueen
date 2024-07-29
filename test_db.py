fca1cce1b257f8b8595fa7e308bb0c0aa12cba15
McQceen <124315125+McQceen@users.noreply.github.com>
2024-07-29 22:56:16 +0000

    Jul 29, 2024, 10:56 PM

--- test_db.py
+++ test_db.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
 from sqlalchemy.orm import relationship
-from sqlalchemy.orm import Base
+from sqlalchemy import Base
 
 pg_engine = create_engine(
     "posthgresql://postgres:123456@localhost:5432/postgres"
@@ -13,4 +13,9 @@ pg_engine = create_engine(
 
 
 class Cars(Base):
+    """Go away pep8."""
+
     __tablename__ = "cars"
+
+    transmission: Mapped[str] = mapped_column(str(255))
+    make: Mapped[str] = mapped_column(str(255))
