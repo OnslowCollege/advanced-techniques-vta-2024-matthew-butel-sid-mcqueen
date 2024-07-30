710f2ae63a0dc888e9b47e6397b0b0006f83ecee
McQceen <124315125+McQceen@users.noreply.github.com>
2024-07-30 23:36:23 +0000

    Jul 30, 2024, 11:36 PM

--- test_db.py
+++ test_db.py
@@ -9,6 +9,7 @@ from sqlalchemy.orm import relationship
 pg_engine = create_engine(
     "postgresql://postgres:123456@localhost:5432/postgres"
 )
+pg_engine.connect()
 
 class Base(DeclarativeBase):
     pass
