05fc7db6be6ccef80ac2b520ad55f13413e53471
McQceen <124315125+McQceen@users.noreply.github.com>
2024-07-30 23:39:07 +0000

    Jul 30, 2024, 11:39 PM

--- test_db.py
+++ test_db.py
@@ -1,10 +1,13 @@
 """OKOKOKOK."""
 
-from sqlalchemy import create_engine, Integer, String
-from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy.orm import Mapped
-from sqlalchemy.orm import mapped_column
-from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine, Integer, select, String
+from sqlalchemy.orm import (
+    DeclarativeBase,
+    Mapped,
+    mapped_column,
+    relationship,
+    Session,
+)
 
 pg_engine = create_engine(
     "postgresql://postgres:123456@localhost:5432/postgres"
@@ -28,3 +31,5 @@ class Cars(Base):
     price: Mapped[int] = mapped_column(Integer)
 
 Base.metadata.create_all(pg_engine)
+
+query = select(Cars)
