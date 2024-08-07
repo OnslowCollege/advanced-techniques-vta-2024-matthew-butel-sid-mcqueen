4d9a031df63f286bc178d172569ddde9373bdf3d
McQceen <124315125+McQceen@users.noreply.github.com>
2024-08-07 09:38:48 +0000

    Aug 7, 2024, 9:38 AM

--- test_db.py
+++ test_db.py
@@ -47,6 +47,6 @@ cars_info: list[str] = [""]
 with Session(pg_engine) as session:
     result = session.execute(query)
     for row in result:
-        cars_info = row.split(" ")
+        cars_info = row.split()
         cars_make.append(cars_info[0])
     print(cars_make)
