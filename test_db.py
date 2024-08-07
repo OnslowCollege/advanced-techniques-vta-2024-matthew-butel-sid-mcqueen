dfe8cbc8914943b9fc7d4d8d1d2af76b4699252c
McQceen <124315125+McQceen@users.noreply.github.com>
2024-08-07 21:23:03 +0000

    Aug 7, 2024, 9:23 PM

--- test_db.py
+++ test_db.py
@@ -41,12 +41,13 @@ class Cars(Base):
 Base.metadata.create_all(pg_engine)
 
 query = select(Cars)
-cars_make: list[str] = [""]
-cars_info: list[str] = [""]
+cars_make: list[str] = []
+cars_info: list[str] = []
+cars_info_transision: str = ""
 with Session(pg_engine) as session:
     result = session.execute(query)
     for row in result:
         if row != "":
             cars_info = row[0]
-            cars_make.append(cars_info[0])
-    print(cars_make)
+            cars_make.append(row[0])
+    print(cars_make[0])
