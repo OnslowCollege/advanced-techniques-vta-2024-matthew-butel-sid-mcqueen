893108362b42292a79333132179459312bacf3b4
MatttthewButel <124315158+MatttthewButel@users.noreply.github.com>
2024-08-12 09:31:06 +1200

    sure

--- testingdb.py
+++ testingdb.py
@@ -9,9 +9,9 @@ from sqlalchemy.orm import (
     Session,
 )
 
-pg_engine = create_engine(
-    "postgresql://postgres:123456@localhost:5432/postgres"
-)
+#pg_engine = create_engine(
+#    "postgresql://postgres:123456@localhost:5432/postgres"
+#)
 
 class Car:
     def __init__(self, transmission: str, make: str, model: str, year_made: str, mileage: int, price: int, ids: int = None):
@@ -88,11 +88,11 @@ class Cars(Base):
 
 
 
-Base.metadata.create_all(pg_engine)
+#Base.metadata.create_all(pg_engine)
 
-query = select(Cars)
+#query = select(Cars)
 
-with Session(pg_engine) as session:
-    result = session.execute(query)
-    for row in result:
-        print(row)
+#with Session(pg_engine) as session:
+#    result = session.execute(query)
+#    for row in result:
+#        print(row)
