0c3c31a94cf8242d10261bd8da796a10cacf86f1
MatttthewButel <124315158+MatttthewButel@users.noreply.github.com>
2024-08-16 08:39:38 +0000

    Aug 16, 2024, 8:39 AM

--- orders_db.py
+++ orders_db.py
@@ -1,4 +1,13 @@
-from sqlalchemy import create_engine, Integer, select, String, insert, MetaData
+from sqlalchemy import (
+    create_engine,
+    Integer,
+    select,
+    String,
+    insert,
+    Date,
+    MetaData,
+    ForeignKey,
+)
 from sqlalchemy.orm import (
     DeclarativeBase,
     Mapped,
@@ -8,6 +17,9 @@ from sqlalchemy.orm import (
     sessionmaker,
 )
 import sqlalchemy as sa
+from users_db import User_Info
+from datetime import date
+
 
 pg_engine = create_engine(
     "postgresql://postgres:123456@localhost:5432/postgres"
@@ -20,36 +32,28 @@ class Base(DeclarativeBase):
     pass
 
 
-class User_Info(Base):
+class Order(Base):
     """Go away pep8."""
 
-    __tablename__ = "user_info"
+    __tablename__ = "order"
 
-    ids: Mapped[int] = mapped_column(Integer, primary_key=True)
-    username: Mapped[str] = mapped_column(String(255))
-    password: Mapped[str] = mapped_column(String(255))
-    card_number: Mapped[str] = mapped_column(String(255))
-    scc: Mapped[int] = mapped_column(Integer)
-    card_name: Mapped[str] = mapped_column(String(255))
-    expire_date: Mapped[str] = mapped_column(String(255))
+    id: Mapped[int] = (mapped_column(Integer, primary_key=True),)
+    date: Mapped[date] = (mapped_column(Date),)
+    user_id: Mapped[int] = mapped_column(Integer, ForeignKey("user_info.ids"))
+
+    # Relationship to UserInfo
+    user: Mapped["UserInfo"] = relationship(
+        "UserInfo", back_populates="orders"
+    )
 
     def __init__(
         self,
-        username: str,
-        password: str,
-        card_number: str,
-        scc: int,
-        card_name: str,
-        expire_date: str,
-        ids: int = None,
+        user: User_Info,
+        id: int = None,
     ):
-        self.username = username
-        self.password = password
-        self.card_number = card_number
-        self.scc = scc
-        self.card_name = card_name
-        self.expire_date = expire_date
-        self.ids = ids
+        self.date = date
+        self.user = user
+        self.id = id
 
 
 class Users:
