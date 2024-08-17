f025c40d523e60f373727cecc76d1e3093a826ad
MatttthewButel <124315158+MatttthewButel@users.noreply.github.com>
2024-08-17 03:35:16 +0000

    Aug 17, 2024, 3:35 AM

--- main.py
+++ main.py
@@ -322,8 +322,6 @@ class UI(App):
             car_price = GUI.Label("$" + repr(car.price))
             car_button: GUI.Button
 
-            print(str(car.ids))
-            print(str(self.is_car_in_cart(car)))
             if self.is_car_in_cart(car):
                 car_button = GUI.Button("In Cart")
                 car_button.set_enabled(False)
